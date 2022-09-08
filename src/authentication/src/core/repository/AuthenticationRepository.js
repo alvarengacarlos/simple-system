1ad031e2af2e459866af4360c6c2f2f093da2c4c
alvarengacarlos <carlosho200@hotmail.com>
2022-09-07 21:48:09 -0400

    delete: Removes the unnecessary files

--- src/authentication/src/core/repository/AuthenticationRepository.js
+++
@@ -1,9 +0,0 @@
-export default class AuthenticationRepository {
-    
-    constructor(databaseConnection) {
-        this._databaseConnection = databaseConnection;
-    }
-
-    searchAccountByEmailAndPassword(email, password) {}
-    
-}
