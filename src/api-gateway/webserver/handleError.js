2b640234702b6ea75e58b4ec342c538e016ceb5a
alvarengacarlos <carlosho200@hotmail.com>
2022-09-30 21:02:46 -0400

    feat: Adds general exception to all errors in case of the application does not handle it

--- src/api-gateway/webserver/handleError.js
+++ src/api-gateway/webserver/handleError.js
@@ -1,7 +1,10 @@
+import Exception from "../../helper/Exception.js";
+
 export default function handleError(error, request, response, next) {
     if (error.httpStatusCode) {
         return response.status(error.httpStatusCode).send({error: error.message});
     }
 
-    return response.status(500).send({error: error.message});
+    const exception = new Exception("sorry, internal error", 11, 500);
+    return response.status(exception.httpStatusCode).send({error: exception.message});
 }
