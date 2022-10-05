8d0d371aa8fb091a754dbd73e543720db3eebc25
alvarengacarlos <carlosho200@hotmail.com>
2022-10-04 22:23:55 -0400

    feat: Implements dependency to start application with database

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,18 @@
 import CleanerService from "./service/cleaner/CleanerService.js";
 import apiGateway from "./api-gateway/index.js";
+import ConnectionMongoDb from "./infrasctructure/database/ConnectionMongoDb.js";
+import Logger from "./util/Logger.js";
 
 export default function initBootStrap() {    
-    apiGateway();
+    ConnectionMongoDb.getConnection()
+    .then(() => {
+        apiGateway();    
 
-    const cleanerService = new CleanerService();
-    cleanerService.initCleanerForTemporaryAccount().initCleanerForLoginAndLogout();
+        const cleanerService = new CleanerService();
+        cleanerService.initCleanerForTemporaryAccount().initCleanerForLoginAndLogout();
+    })
+    .catch((exception) => {
+        Logger.errorLog("Sorry, the appliation can't start", exception);
+    })
+    
 }
