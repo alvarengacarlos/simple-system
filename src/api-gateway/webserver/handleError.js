e1a4f3c0161433ae6eeba18c8699912144eb415a
alvarengacarlos <carlosho200@hotmail.com>
2022-10-09 17:53:12 -0400

    fix: Fixes the http status code

--- src/api-gateway/webserver/handleError.js
+++ src/api-gateway/webserver/handleError.js
@@ -1,10 +1,10 @@
 import Exception from "../../helper/Exception.js";
 
 export default function handleError(error, request, response, next) {
-    if (error.httpStatusCode) {
-        return response.status(error.httpStatusCode).send({error: error.message});
+    if (error._httpStatusCode) {
+        return response.status(error._httpStatusCode).json({error: error.message});
     }
 
     const exception = new Exception("sorry, internal error", 11, 500);
-    return response.status(exception.httpStatusCode).send({error: exception.message});
+    return response.status(exception._httpStatusCode).json({error: exception.message});
 }
