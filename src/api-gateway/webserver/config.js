d9c1e1c61ff3273963f054265ae7f370010a1793
alvarengacarlos <carlosho200@hotmail.com>
2022-10-12 10:40:25 -0400

    feat: Configures the static files in express framework

--- src/api-gateway/webserver/config.js
+++ src/api-gateway/webserver/config.js
@@ -1,9 +1,20 @@
 import process from "process";
 import express from "express";
+import path from "path";
 
 export default function config(app) {
     app.use(express.json());
+    defineStaticFiles(app);
     defineEnviromentType(app);
+}
+
+function defineStaticFiles(app) {
+    if (!process.env.APPLICATION_ROOT_DIR) {
+        throw new Error("The APPLICATION_ROOT_DIR environment variable is not defined");
+    }
+
+    const p = path.resolve(process.env.APPLICATION_ROOT_DIR, "src", "public");
+    app.use(express.static(p));
 }
 
 function defineEnviromentType(app) {
