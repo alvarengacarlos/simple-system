c8d968b515e9575d3c2884b2e6cea275756431fa
alvarengacarlos <carlosho200@hotmail.com>
2022-10-12 15:26:42 -0400

    feat: Moves the ValidationException class to helper module

--- src/account/webserver/Validation.js
+++ src/account/webserver/Validation.js
@@ -1,15 +1,5 @@
 import Joi from "joi";
-
-class ValidationException extends Error {
-    
-    constructor(error) {
-        const message = error.details.map((element) => element.message);
-        
-        super(JSON.stringify({message: message}));
-        this._httpStatusCode = 400;
-    }
-
-}
+import ValidationException from "../../helper/ValidationException.js";
 
 export default class Validation {
 
