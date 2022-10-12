16f3c7453d566524132e89d299c9a02aa68b9d72
alvarengacarlos <carlosho200@hotmail.com>
2022-10-12 09:49:05 -0400

    feat: Add comments into logger

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,15 +1,18 @@
 export default class Logger {
 
+    //Use it to log general informations
     static infoLog(message) {
         const currentDate = new Date().toLocaleString();
         console.log(`[INFO] -> ${message} - ${currentDate}`);
     }
 
+    //Use it to log errors or something like that and not stop the application execution
     static warningLog(message) {
         const currentDate = new Date().toLocaleString();
         console.warn(`[WARNING] -> ${message} - ${currentDate}`);
     }
 
+    //Use it to log erros that stop the application execution
     static errorLog(message, error) {
         const currentDate = new Date().toLocaleString();
         console.error(`[ERROR] -> ${message} - "${error.message}" - '${currentDate}'`);
